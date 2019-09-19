8b051fe06482cb7ce1ae3ca3e27286c101f5d705
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2019-09-19 10:38:00 -0500

    inicio

--- app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_ejercicio9/MainActivity.kt
+++ app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_ejercicio9/MainActivity.kt
@@ -9,6 +9,11 @@ class MainActivity : AppCompatActivity() {
 
     var boton : Button?=null
     var tabla : TextView?=null
+    var numeromul : TextView?=null
+    var numerodos: TextView?=null
+    var contador =0
+    var mensage =""
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +22,16 @@ class MainActivity : AppCompatActivity() {
 
         boton=findViewById(R.id.boton)
         tabla=findViewById(R.id.tabla)
+        numeromul=findViewById(R.id.numeromu)
+        numerodos = findViewById(R.id.numerodo)
 
+
+
+        boton?.setOnClickListener {
+
+            
+
+        }
 
     }
 }
