baede2b4008cd5a0d17052e252c23a617414f239
PaulBrR <paalbrisenoro@ittepic.edu.mx>
2019-09-21 14:54:14 -0500

    #NoMeRepruebesBenigno

--- app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_ejercicio9/MainActivity.kt
+++ app/src/main/java/paulbriseno/ittepi/edu/mx/tpdm_u1_ejercicio9/MainActivity.kt
@@ -3,7 +3,9 @@ package paulbriseno.ittepi.edu.mx.tpdm_u1_ejercicio9
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,10 @@ class MainActivity : AppCompatActivity() {
     var numeromul : TextView?=null
     var numerodos: TextView?=null
     var contador =0
-    var mensage =""
+    var wow = 0
+    var calculos = 0
+    var aux =0
+    var layo : LinearLayout?=null
 
 
 
@@ -27,10 +32,37 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         boton?.setOnClickListener {
+            layo=findViewById(R.id.layin)
+            layo?.removeAllViews()
+            var vectoroper=Vector<TextView>()
+            aux = numeromul?.text.toString().toInt()
+            wow=numerodos?.text.toString().toInt()
+            var vector = numeromul?.text.toString()
 
-            
+            if (wow>=aux){
+                (1..wow).forEach {
 
+
+                    if (wow >= 0) {
+
+
+                        calculos= aux*contador
+
+                        var rbd = TextView(this)
+                        rbd.setText("$aux"+"x"+"$it"+"="+calculos)
+                        rbd.textSize= 20F
+                        layo?.addView(rbd)
+                    }
+                    contador++
+
+                }
+            }
+
+
+            numeromul?.setText("")
+            numerodos?.setText("")
         }
 
     }
